9d48be610ea49979b2aa6c3a43eec87a327a45f0
Stepan-Prysakar <s.v.prysakar@gmail.com>
2026-01-09 14:00:29 +0200

    update

--- HW_1_Task_2.py
+++ HW_1_Task_2.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
 
-    if min>=1 and max<=1000 and min <= quantity <= max: # перевірка коректності вхідних даних
+    if min>=1 and max<=1000 and (max-min) >= quantity: # перевірка коректності вхідних даних та забезпечення умови унікальності для random.sample
         lottery_numbers = sorted(random.sample(range(min, max), quantity)) # відсортований список випадкових чисел
         return lottery_numbers
     else:
