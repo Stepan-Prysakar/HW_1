ce271c59ab3fb0b157179b15be0ac8fd80a0831e
Stepan-Prysakar <s.v.prysakar@gmail.com>
2026-01-09 12:34:40 +0200

    change of condition for quantity

--- HW_1_Task_2.py
+++ HW_1_Task_2.py
@@ -2,10 +2,10 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
 
-    if min>=1 and max<=1000 and min <= quantity <= max: # перевірка коректності вхідних даних
+    if min>=1 and max<=1000 and (max-min) >= quantity: # перевірка коректності вхідних даних та забезпечення унікальності значень для random.sample
         lottery_numbers = sorted(random.sample(range(min, max), quantity)) # відсортований список випадкових чисел
         return lottery_numbers
     else:
         return [] # вивід при невідповідності вхідних параметрів
 
-print("Ваші лотерейні числа:", get_numbers_ticket(1, 60, 5))
+print("Ваші лотерейні числа:", get_numbers_ticket(5, 40, 10))
