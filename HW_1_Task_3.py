0581e92e3b79df4454784175562e759c98a7bfb9
Stepan-Prysakar <s.v.prysakar@gmail.com>
2026-01-09 14:02:46 +0200

    update

--- HW_1_Task_3.py
+++ HW_1_Task_3.py
@@ -5,32 +5,15 @@ def normalize_phone(phone_number):
     # Видаляємо зайвий синтаксис:
     patern_dell_symbols = r"[\[\];,\-:!\.tnrfn\\=\s()]" # як додати в шаблон видалення всіх букв? (a-zA-Z - тільки латинські)?
     repl_dell_symbols = ''
-    phone_number_dell_symbols = re.sub(patern_dell_symbols, repl_dell_symbols, str(phone_number))
-  
-    # Перетворюємо рядок phone_number_dell_symbols на список рядкових об'єктів, по яких буде проходити цикл for для подальшого форматування:
-    patern_list = r'[\']+'
-    phone_number_list = re.split(patern_list, phone_number_dell_symbols)[1:-1] # [1:-1] - ігноруємо елементи ('') на початку та вкінці
-   
-    # Ініціалізуємо список та наповнюємо його відформатованими номерами:
-    list_numbers = []
-    for num in phone_number_list:
-        num = num.removeprefix('8') # видаляємо можливі варіанти запису коду країни, приводячи всі номери до одного формату
-        num = num.removeprefix('38')
-        num = num.removeprefix('+38')
-        num = '+38' + num # додаємо однаковий формат запису коду країни для всіх номерів
-        list_numbers+=[num]
-    return list_numbers
+    number = re.sub(patern_dell_symbols, repl_dell_symbols, phone_number)
+    if number.startswith('+380'):
+        return number
+    else:
+        number = number.removeprefix('8')
+        number = number.removeprefix('38')
+        number = number.removeprefix('+38')
+        number = '+38' + number
+        return number
 
-
-raw_numbers = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
+raw_numbers = "38050 111 22 11  "
 print(normalize_phone(raw_numbers))
